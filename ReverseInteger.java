6d3ff2a21b629de3a6214c8e3f5594508a545cc1
Umang kumar <112698874+UmangKumar21@users.noreply.github.com>
2022-11-09 01:00:22 +0530

    Create ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int u = sc.nextLine();
+    int g=0;
+    while(u != 0){
+      g = g*10+k;
+      u=u/10;
+    }
+    System.out.print(g);
+  }
+}
